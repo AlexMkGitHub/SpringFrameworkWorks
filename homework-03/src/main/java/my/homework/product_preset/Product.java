25411ca8fe7a76ce0ab50835419f712c05a28b6d
alexm <mkalex77@gmail.com>
2022-04-08 03:02:06 +0700

    Практическая работа 3. Осталось задействовать кнопку Add Product.

--- homework-03/src/main/java/my/homework/product_preset/Product.java
+++ homework-03/src/main/java/my/homework/product_preset/Product.java
@@ -5,12 +5,10 @@ public class Product {
     private Long id;
     private String title;
     private double cost;
-    private String ruTitle;
 
-    public Product(String title, double cost, String ruTitle) {
+    public Product(String title, double cost) {
         this.title = title;
         this.cost = cost;
-        this.ruTitle = ruTitle;
     }
 
     public Long getId() {
@@ -37,11 +35,4 @@ public class Product {
         this.cost = cost;
     }
 
-    public String getRuTitle() {
-        return ruTitle;
-    }
-
-    public void setRuTitle(String ruTitle) {
-        this.ruTitle = ruTitle;
-    }
 }
