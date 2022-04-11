92313fec4e1a483d64b58b9c69b0596c1b5d3f54
alexm <mkalex77@gmail.com>
2022-04-11 22:56:34 +0700

    Практическая работа 4. Перенос файлов из предыдущего задания.

---
+++ homework-04/src/main/java/my/homework/controller/ProductController.java
@@ -0,0 +1,47 @@
+package my.homework.controller;
+
+import my.homework.product_preset.Product;
+import my.homework.product_preset.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/product")
+@Controller
+public class ProductController {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping
+    public String listProduct(Model model) {
+        model.addAttribute("products", productRepository.findAll());
+        return "product";
+    }
+
+    @GetMapping("/{id}")
+    public String productForm(@PathVariable("id") long id, Model model) {
+        model.addAttribute("product", productRepository.findById(id));
+        return "product_form";
+    }
+
+    @PostMapping
+    public String productSave(Product product) {
+        productRepository.save(product);
+        return "redirect:/product";
+    }
+
+    @GetMapping("/new")
+    public String newForm(Model model) {
+        model.addAttribute("product", productRepository.save(new Product("", 0)));
+        return "product_form";
+    }
+}
